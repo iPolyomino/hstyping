a297baa1d5e0fc11c59c5f2d35d9a186a38d7546
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2023-02-10 18:45:54 +0900

    format code

--- typingGame.hs
+++ typingGame.hs
@@ -1,45 +1,47 @@
-import System.IO (IOMode(..), withFile, hGetLine, hIsEOF)
-import Data.Time (getCurrentTime, diffUTCTime)
+import Data.Char (isSpace)
 import Data.List (genericLength)
+import Data.Maybe (catMaybes)
+import Data.Time (diffUTCTime, getCurrentTime)
 import Data.Traversable (for)
 import System.Environment (getArgs)
-import Text.Read (readMaybe)
-import Data.Char (isSpace)
-import Data.Maybe (catMaybes)
+import System.IO (IOMode (..), hGetLine, hIsEOF, withFile)
 import System.Random (getStdRandom, randomR)
+import Text.Read (readMaybe)
 
 typingLines = 5
+
 rankingFile = "ranking.txt"
 
-doWhile :: Monad m => m (Maybe a) -> m [a]
+doWhile :: (Monad m) => m (Maybe a) -> m [a]
 doWhile m = do
-        mx <- m
-        case mx of
-                Just x -> (x :) <$> doWhile m
-                Nothing -> return []
+  mx <- m
+  case mx of
+    Just x -> (x :) <$> doWhile m
+    Nothing -> return []
 
 fileLines :: FilePath -> IO [String]
 fileLines fp = withFile fp ReadMode $ \h -> doWhile $ do
-        b <- hIsEOF h
-        if b then return Nothing else Just <$> hGetLine h
+  b <- hIsEOF h
+  if b then return Nothing else Just <$> hGetLine h
 
 generateProblem :: [String] -> IO [String]
-generateProblem lines = randomRange >>=
-        \i -> return $ take typingLines $ drop i lines
-        where
-        randomRange = getStdRandom $ randomR (0, length lines - typingLines)
+generateProblem lines =
+  randomRange
+    >>= \i -> return $ take typingLines $ drop i lines
+  where
+    randomRange = getStdRandom $ randomR (0, length lines - typingLines)
 
 getLineWithTime :: IO (String, Integer)
 getLineWithTime = do
-        t1 <- getCurrentTime
-        s <- getLine
-        t2 <- getCurrentTime
-        return (s, ceiling $ t2 `diffUTCTime` t1)
+  t1 <- getCurrentTime
+  s <- getLine
+  t2 <- getCurrentTime
+  return (s, ceiling $ t2 `diffUTCTime` t1)
 
 countMiss :: String -> String -> Integer
-countMiss (c : cs) (d :ds)
-        | c == d = countMiss cs ds
-        | otherwise = 1 + countMiss cs ds
+countMiss (c : cs) (d : ds)
+  | c == d = countMiss cs ds
+  | otherwise = 1 + countMiss cs ds
 countMiss cs [] = genericLength cs
 countMiss [] ds = genericLength ds
 
@@ -56,35 +58,35 @@ fromRecord (n, p) = n ++ " " ++ show p
 
 insertRecord :: Integer -> [Record] -> [(Maybe String, Integer)]
 insertRecord px rrs@((nr, pr) : rs)
-        | px >= pr = (Nothing, px) : map (\(n, p) -> (Just n, p)) rrs
-        | otherwise = (Just nr, pr) : insertRecord px rs
+  | px >= pr = (Nothing, px) : map (\(n, p) -> (Just n, p)) rrs
+  | otherwise = (Just nr, pr) : insertRecord px rs
 insertRecord px [] = [(Nothing, px)]
 
 yourName :: [(Maybe String, Integer)] -> IO [Record]
 yourName ((Just n, p) : rs) = ((n, p) :) <$> yourName rs
 yourName ((Nothing, p) : rs) = do
-        putStrLn "What's your name?"
-        n <- getLine
-        ((filter (not . isSpace) n, p) :) <$> yourName rs
+  putStrLn "What's your name?"
+  n <- getLine
+  ((filter (not . isSpace) n, p) :) <$> yourName rs
 yourName [] = return []
 
 ranking :: FilePath -> Integer -> IO ()
 ranking fp p = do
-        rs <- catMaybes . map record <$> fileLines fp
-        rs' <- yourName . take 10 $ insertRecord p rs
-        writeFile fp . unlines $ map fromRecord rs'
+  rs <- catMaybes . map record <$> fileLines fp
+  rs' <- yourName . take 10 $ insertRecord p rs
+  writeFile fp . unlines $ map fromRecord rs'
 
 main :: IO ()
 main = do
-        fp : _ <- getArgs
-        line <- fileLines fp
-        pro <- generateProblem line
-        ps <- for pro $ \l -> do
-                putStrLn l
-                (s, sc) <- getLineWithTime
-                let p = point l s sc
-                print p
-                return p
-        let rslt = sum ps `div` genericLength ps
-        print rslt
-        ranking rankingFile rslt
+  fp : _ <- getArgs
+  line <- fileLines fp
+  pro <- generateProblem line
+  ps <- for pro $ \l -> do
+    putStrLn l
+    (s, sc) <- getLineWithTime
+    let p = point l s sc
+    print p
+    return p
+  let rslt = sum ps `div` genericLength ps
+  print rslt
+  ranking rankingFile rslt
